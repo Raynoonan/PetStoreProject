1fdbde869633ec0af742c096319b961d492b2cfd
Ray Noonan <raynoonan03@gmail.com>
2024-12-20 14:30:46 -0500

    Week 14

---
+++ src/main/java/pet/store/controller/model/PetStoreCustomer.java
@@ -0,0 +1,23 @@
+package pet.store.controller.model;
+
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pet.store.entity.PetStore;
+
+@Data
+@NoArgsConstructor
+public class PetStoreCustomer {
+
+  private Long customerId;
+  private String customerFirstName;
+  private String customerLastName;
+  private String customerEmail;
+  
+  private Set<PetStore> petStores = new HashSet<>();
+  
+  PetStoreCustomer(Object Customer){
+   
+  }
+}
---
+++ src/main/java/pet/store/controller/model/PetStoreEmployee.java
@@ -0,0 +1,22 @@
+package pet.store.controller.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pet.store.entity.PetStore;
+
+@Data
+@NoArgsConstructor
+public class PetStoreEmployee {
+
+  private Long employeeId;
+  private String employeeFirstName;
+  private String employeeLastName;
+  private String employeePhone;
+  private String employeeJobTitle;
+  
+  private PetStore petStore;
+  
+  PetStoreEmployee(Object Employee){
+    
+  }
+}
